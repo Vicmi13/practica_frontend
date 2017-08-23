7108001f260c03a376ca93411696e29190d43be2
Victor Torres V <vicm_13@hotmail.com>
2017-08-23 07:15:33 -0500

    Se agregan cosas al main-header ---->Noche no productible

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,17 @@
 window.$ = window.jQuery = require("jquery");
 document.write(console.log('Entrando a main.js'));
+
+
+const $ =require("jquery");
+
+    //Script para esconder header
+    function toggleMobileMenu() {
+        var $mobileMenu = $('#mobile-main-menu');   
+         $mobileMenu.slideToggle('fast');
+     };
+
+     $(document).ready(function() {
+         $('#mobile-menu-button').on('click', toggleMobileMenu);
+    });
+
+     
