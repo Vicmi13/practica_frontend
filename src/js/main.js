0abd0d4e5a0f698d7138940dbf9502d8828cf325
Miguel Torres <vicmmi.13@gmail.com>
2017-08-30 18:55:36 -0500

    Se agrega formulario,local storage y material design

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,18 @@ document.write(console.log('Entrando a main.js'));
 
 
 const $ =require("jquery");
+    
+        $( "#target" ).click(function() {
+                if (localStorage.clickcount) {
+                    localStorage.clickcount = Number(localStorage.clickcount)+1;
+                } else {
+                localStorage.clickcount = 0;
+                 }
+                 document.getElementById("like").innerHTML =" " +localStorage.clickcount;
+                 $("#like").css({"font-size": "12px", "border-radius": "50%", "background": "#ff4081",
+                                 "color": "#fff", "position": "absolute", "padding-bottom": "2px;",
+                                 "width": "22px", "heigth": "22px", "text-align": "center"});
+        });
 
     //Script para esconder header
     function toggleMobileMenu() {
@@ -15,4 +27,7 @@ const $ =require("jquery");
          $('#mobile-menu-button').on('click', toggleMobileMenu);
     });
 
+    
+
      
+   
