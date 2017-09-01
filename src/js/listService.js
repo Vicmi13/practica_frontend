896df15df55f2aa3983a57e91718705712267d6c
Miguel Torres <vicmmi.13@gmail.com>
2017-09-01 18:31:35 -0500

    Se agrega listService y estado de peticiones de comentarios

---
+++ src/js/listService.js
@@ -0,0 +1,35 @@
+const $ = require('jquery');
+ 
+export default class listService{
+
+    constructor(url){
+        this.url = url;
+    }
+
+    getComentarios(successCallback, errorCallback){
+        $.ajax({
+            url: `${this.url}`,
+            success : successCallback,
+            error : errorCallback
+        });
+    }
+
+    saveComentario(comentario, successCallback, errorCallback){
+        $.ajax({
+            url: `${this.url}`,
+            method: 'post',
+            data: comentario,
+            success: successCallback,
+            error: errorCallback
+        });
+    }
+
+    
+    getDetailComent(comentarioId, successCallback, errorCallback){
+         $.ajax({
+            url: `${this.url}${comentarioId}`,
+            success: successCallback,
+            error: errorCallback
+        });
+    }
+ }
